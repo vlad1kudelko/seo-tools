d74730a814765b39004b18ed3376d8bd90669bbf
Vlad Kudelko <vlad1kudelko@gmail.com>
2025-10-19 21:49:46 +0800

    -

--- ahrefs-cluster/main.py
+++ ahrefs-cluster/main.py
@@ -5,6 +5,7 @@ import sys
 # на основе выгрузки Ahrefs
 
 def main(file):
+    res = ''
     with open(file, encoding='utf-16') as f:
         reader = csv.reader(f, delimiter='\t')
         count = 0
@@ -16,30 +17,36 @@ def main(file):
                 row = dict(zip(keys, row))
                 if row['Country'] != 'RU': continue
                 if row['Location'] != 'Russian Federation': continue
-                if row['Current URL'] not in cluster:
-                    cluster[row['Current URL']] = []
-                cluster[row['Current URL']].append({
+                curl = row['Current URL']
+                if curl == '':
+                    curl = row['Previous URL']
+                if curl not in cluster:
+                    cluster[curl] = []
+                cluster[curl].append({
                     'Keyword':  row['Keyword'],
                     'Volume':   row['Volume'],
                     'KD':       row['KD'],
                     'traffic':  row['Current organic traffic'],
                 })
                 count += 1
-        print(count)
+        res += str(count)
         for url, rows in cluster.items():
-            print()
-            print([url])
+            res += '\n'
+            res += str([url]) + '\n'
             for row in rows:
-                print(' '*4 + ' '.join([
-                    'V: '  + row['Volume'] .rjust(4),
-                    'KD: ' + row['KD']     .rjust(4),
-                    'T: '  + row['traffic'].rjust(4),
+                res += (' '*4 + ' '.join([
+                    'V: '  + row['Volume' ].rjust(4, '_'),
+                    'KD: ' + row['KD'     ].rjust(4, '_'),
+                    'T: '  + row['traffic'].rjust(4, '_'),
                     'K: '  + row['Keyword'],
-                ]))
+                ]) + '\n')
+        return res
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print('Usage: python main.py <file>')
         sys.exit(1)
     else:
-        main(sys.argv[1])
+        print_str = main(sys.argv[1])
+        with open('out.txt', 'w') as f:
+            f.write(print_str)
