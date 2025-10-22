97f42eea02e2e3fed0bb61116a4373f9b56a3965
cooler <you@example.com>
2025-10-23 00:46:58 +0800

    -

--- ai-detector/main.py
+++ ai-detector/main.py
@@ -3,8 +3,28 @@ import os
 import requests
 import sys
 
+#  https://www.scribbr.com/ai-detector/
+
 load_dotenv()
 connect_sid = os.getenv('CONNECT_SID')
+
+def get_md(inp_file):
+    ret = []
+    with open(inp_file) as f:
+        lines = f.readlines()
+        lines = list(map(str.rstrip, lines))
+        if len(lines) > 0 and lines[0] == '---':
+            is_toml = True
+            for line in lines[1:]:
+                if line == '---':
+                    is_toml = False
+                    continue
+                else:
+                    if not is_toml:
+                        ret.append(line)
+        else:
+            raise Exception('ERROR parse')
+    return '\n'.join(ret)
 
 def test_ai(inp_text):
     api_url = 'https://quillbot.scribbr.com/api'
@@ -29,12 +49,27 @@ def test_ai(inp_text):
     )
     assert response.status_code == 200, print('ERROR_2', response.json())
     pers = response.json()['data']['value']['aiScore'] * 100
-    print([ pers ])
+    return [ int(pers) ]
 
-if __name__ == '__main__':
+def test_ai_wrap(inp_file):
+    inp_text = get_md(inp_file)
+    inp_text = ' '.join(inp_text.split()[:1200])
+    return test_ai(inp_text)
+
+def test_dir(inp_dir):
+    for root, dirs, files in os.walk(inp_dir):
+        for file in files:
+            filename = f'{root}/{file}'
+            print(test_ai_wrap(filename), filename)
+
+def main():
     if len(sys.argv) > 1:
-        with open(sys.argv[1]) as f:
-            inp_text = f.read()
-        test_ai(inp_text)
+        if os.path.isdir(sys.argv[1]):
+            test_dir(sys.argv[1])
+        else:
+            print(test_ai(sys.argv[1]))
     else:
         print('ERROR_3')
+
+if __name__ == '__main__':
+    main()
